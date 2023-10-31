09106b5c121b2ecbe636e0cad6612c81a006667b
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-31 11:57:33 +0100

    Fix RobotWithTwoArms

--- 02-inheritance-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
+++ 02-inheritance-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
@@ -3,49 +3,64 @@ package it.unibo.design.robot.impl;
 import it.unibo.design.robot.api.BasicArm;
 import it.unibo.design.robot.api.RobotWithArms;
 
-public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
+public class RobotWithTwoArms extends BaseRobot implements RobotWithArms {
 
     private static final double ARMS_FULL_CUNSUPTION = 0.3;
+    private static final double NUMBER_OF_ARMS = 2;
 
-    private BasicArm armRight = new BasicArm();
-    private BasicArm armLeft = new BasicArm();
+    private BasicArm armRight;
+    private BasicArm armLeft;
     private int carriedItems;
 
     public RobotWithTwoArms(String robotName) {
         super(robotName);
+        armRight = new BasicArm();
+        armLeft = new BasicArm();
         this.carriedItems=0;
     }
 
+    private BasicArm firstFreeArm(){
+        if(!armRight.isGrabbing()){
+            return armRight;
+        } else if(!armLeft.isGrabbing()){
+            return armLeft;
+        }
+        return null;
+    }
+
+    private BasicArm firstOccupedArm() {
+        if(armRight.isGrabbing()){
+            return armRight;
+        } else if(armLeft.isGrabbing()){
+            return armLeft;
+        }
+        return null;
+    }
+
+    private void pick(BasicArm arm) {
+        arm.pickUp();
+        this.carriedItems++;
+        super.consumeBattery(BasicArm.getConsumptionForPickUp());
+    }
+
+    private void drop(BasicArm arm) {
+        arm.dropDown();
+        this.carriedItems--;
+        super.consumeBattery(BasicArm.getConsumptionForDropDown());
+    }
+
     public boolean pickUp() {
-        if(super.isBatteryEnough(BasicArm.getConsumptionForPickUp()) && getCarriedItemsCount() < 2){
-            if(!armRight.isGrabbing()){
-                armRight.pickUp();
-                this.carriedItems++;
-                super.consumeBattery(BasicArm.getConsumptionForPickUp());
-                return true;
-            } else if(!armLeft.isGrabbing()){
-                armLeft.pickUp();
-                this.carriedItems++;
-                super.consumeBattery(BasicArm.getConsumptionForPickUp());
-                return true;
-            }
+        if(super.isBatteryEnough(BasicArm.getConsumptionForPickUp()) && getCarriedItemsCount() < NUMBER_OF_ARMS){
+            pick(firstFreeArm());
+            return true;
         }
         return false;
     }
 
     public boolean dropDown() {
-        if(super.isBatteryEnough(BasicArm.getConsumptionForDropDown()) && getCarriedItemsCount() > 0){
-            if(armRight.isGrabbing()){
-                armRight.dropDown();
-                this.carriedItems--;
-                super.consumeBattery(BasicArm.getConsumptionForDropDown());
-                return true;
-            } else if(armLeft.isGrabbing()){
-                armLeft.dropDown();
-                this.carriedItems--;
-                super.consumeBattery(BasicArm.getConsumptionForDropDown());
-                return true;
-            }
+        if(super.isBatteryEnough(BasicArm.getConsumptionForDropDown()) && getCarriedItemsCount() > 0) {
+            drop(firstOccupedArm());
+            return true;
         }
         return false;
     }
