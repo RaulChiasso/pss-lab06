cd80541d872a7db218dbffe7ebad62dfba8f254a
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-31 11:39:12 +0100

    Implement RobotWithTwoArms

--- 02-inheritance-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
+++ 02-inheritance-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
@@ -1,29 +1,61 @@
 package it.unibo.design.robot.impl;
 
+import it.unibo.design.robot.api.BasicArm;
 import it.unibo.design.robot.api.RobotWithArms;
 
 public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
 
+    private static final double ARMS_FULL_CUNSUPTION = 0.3;
+
+    private BasicArm armRight = new BasicArm();
+    private BasicArm armLeft = new BasicArm();
+    private int carriedItems;
+
     public RobotWithTwoArms(String robotName) {
         super(robotName);
+        this.carriedItems=0;
     }
 
-    @Override
     public boolean pickUp() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pickUp'");
+        if(super.isBatteryEnough(BasicArm.getConsumptionForPickUp()) && getCarriedItemsCount() < 2){
+            if(!armRight.isGrabbing()){
+                armRight.pickUp();
+                this.carriedItems++;
+                super.consumeBattery(BasicArm.getConsumptionForPickUp());
+                return true;
+            } else if(!armLeft.isGrabbing()){
+                armLeft.pickUp();
+                this.carriedItems++;
+                super.consumeBattery(BasicArm.getConsumptionForPickUp());
+                return true;
+            }
+        }
+        return false;
     }
 
-    @Override
     public boolean dropDown() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dropDown'");
+        if(super.isBatteryEnough(BasicArm.getConsumptionForDropDown()) && getCarriedItemsCount() > 0){
+            if(armRight.isGrabbing()){
+                armRight.dropDown();
+                this.carriedItems--;
+                super.consumeBattery(BasicArm.getConsumptionForDropDown());
+                return true;
+            } else if(armLeft.isGrabbing()){
+                armLeft.dropDown();
+                this.carriedItems--;
+                super.consumeBattery(BasicArm.getConsumptionForDropDown());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCarriedItemsCount() {
+        return this.carriedItems;
     }
 
     @Override
-    public int getItemsCarried() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getItemsCarried'");
+    protected double getBatteryRequirementForMovement() {
+        return RobotWithTwoArms.MOVEMENT_DELTA_CONSUMPTION + ARMS_FULL_CUNSUPTION * getCarriedItemsCount();
     }
-     
 }
