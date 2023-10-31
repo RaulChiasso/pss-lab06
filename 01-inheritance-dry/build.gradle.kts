7a9d0d04b611c6e7adcd714e606ffa7be382267c
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-31 09:30:02 +0100

    Add plugins in build.gradle.kts

--- 01-inheritance-dry/build.gradle.kts
+++ 01-inheritance-dry/build.gradle.kts
@@ -1,1 +1,10 @@
-plugins { java }
+plugins { 
+    java 
+    application
+    checkstyle
+    }
+repositories { mavenCentral() } // Configura Gradle per cercare e scaricare da Maven Central
+
+application { // Configurazione dell'esecuzione, la main class è sufficiente
+    mainClass.set("it.unibo.inheritance.test.TestBankAccount")
+}
